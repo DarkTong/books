dedd7d478b6f4ed3ba4e222e03d727669f3f362e
darktong <darktong.black@gmail.com>
2019-05-25 15:39:03 +0800

    envirnoment

--- Computer Graphics with OpenGL Fourth Edition/Capture_4/src/main.cpp
+++ Computer Graphics with OpenGL Fourth Edition/Capture_4/src/main.cpp
@@ -19,12 +19,14 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    gladLoadGL();
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
